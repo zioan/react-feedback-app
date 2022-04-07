207f88df2b8313e9bf643d9a1f6d17e64e485c4d
zioan <zaharia.ioan@gmail.com>
2022-04-07 13:37:19 +0200

    basic components with lifting state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackData from './data/FeedbackData'
+import FeedbackList from './components/FeedbackList'
+
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData)
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+
   return (
-    <div className='class'>
-      <h1>App</h1>
-    </div>
+    <>
+      <Header />
+      <div className='container'>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
   )
 }
 
