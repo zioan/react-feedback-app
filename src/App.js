579af8848d150df8a0f8657bc809a0bd91e49cdb
zioan <zaharia.ioan@gmail.com>
2022-04-09 10:53:02 +0200

    refactoring state to useContext

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import { v4 as uuidv4 } from 'uuid'
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   NavLink,
 } from 'react-router-dom'
-import { useState } from 'react'
 import Header from './components/Header'
 import Card from './components/shared/Card'
 import Post from './components/Post'
-import FeedbackData from './data/FeedbackData'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
@@ -18,27 +15,16 @@ import AboutIconLink from './components/AboutIconLink'
 import Redirect from './components/Redirect'
 import RedirectHelper from './components/RedirectHelper'
 import NestedRoutes from './components/NestedRoutes'
+import { FeedbackProvider } from './context/FeedbackContext'
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
-  }
-
-  const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
-    }
-  }
-
   return (
-    // React router 6
-    <Router>
-      <Header />
-      {/* Navigarion example using NavLink */}
-      {/* <Card>
+    <FeedbackProvider>
+      {/* React router 6 */}
+      <Router>
+        <Header />
+        {/* Navigarion example using NavLink */}
+        {/* <Card>
         <NavLink to='/' activeClassName='active'>
           Home
         </NavLink>
@@ -46,37 +32,35 @@ function App() {
           About
         </NavLink>
       </Card> */}
-      <div className='container'>
-        <Routes>
-          <Route
-            exact
-            path='/'
-            element={
-              <>
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList
-                  feedback={feedback}
-                  handleDelete={deleteFeedback}
-                />
-              </>
-            }
-          ></Route>
-          <Route path='/about' element={<AboutPage />} />
+        <div className='container'>
+          <Routes>
+            <Route
+              exact
+              path='/'
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            ></Route>
+            <Route path='/about' element={<AboutPage />} />
 
-          {/* How to send params from route */}
-          <Route path='/post/:id/:name' element={<Post />} />
+            {/* How to send params from route */}
+            <Route path='/post/:id/:name' element={<Post />} />
 
-          {/* Redirect */}
-          <Route path='/redirect' element={<Redirect />} />
-          <Route path='/redirect2' element={<RedirectHelper />} />
+            {/* Redirect */}
+            <Route path='/redirect' element={<Redirect />} />
+            <Route path='/redirect2' element={<RedirectHelper />} />
 
-          {/* Nested routes / components render by route */}
-          <Route path='/nested/*' element={<NestedRoutes />} />
-        </Routes>
-      </div>
-      <AboutIconLink />
-    </Router>
+            {/* Nested routes / components render by route */}
+            <Route path='/nested/*' element={<NestedRoutes />} />
+          </Routes>
+        </div>
+        <AboutIconLink />
+      </Router>
+    </FeedbackProvider>
   )
 }
 
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,8 +1,11 @@
 import { motion, AnimatePresence } from 'framer-motion'
-import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList() {
+  const { feedback } = useContext(FeedbackContext)
+
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>
   }
@@ -17,11 +20,7 @@ function FeedbackList({ feedback, handleDelete }) {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <FeedbackItem
-              key={item.id}
-              item={item}
-              handleDelete={handleDelete}
-            />
+            <FeedbackItem key={item.id} item={item} />
           </motion.div>
         ))}
       </AnimatePresence>
@@ -32,22 +31,11 @@ function FeedbackList({ feedback, handleDelete }) {
   //   <div className='feedback-list'>
   //     {feedback.map((item) => {
   //       return (
-  //         <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //         <FeedbackItem key={item.id} item={item} />
   //       )
   //     })}
   //   </div>
   // )
-}
-
-FeedbackList.propTypes = {
-  // feedback: PropTypes.array
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      // id: PropTypes.number.isRequired,
-      rating: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-    })
-  ),
 }
 
 export default FeedbackList
